d71acff68d818e5ccbc3ffc231fabeb724a745f1
Romanechavarria <romandejesus19@hotmail.com>
2024-09-06 10:04:32 -0500

    avances triangulo

---
+++ Ejercicio2_triangulo/Ejercicio2_triangulo/Program.cs
@@ -0,0 +1,56 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // les los tres segmentos y que muestre si se puede construir un triangulo con estos segmentos
+
+        //declaracion de variables 
+
+        double seg1, seg2, seg3;
+        bool istriangle;
+        string msj;
+
+        //pedir los datos por pantalla
+        Console.Write("ingrese el segmento1");
+        seg1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el segmento2");
+        seg2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el segmento3");
+        seg3 = Convert.ToDouble(Console.ReadLine());
+
+
+        //validacion en una sola linea
+
+       if (((seg1 + seg2) > seg3) && ((seg1 + seg3) > seg2) && ((seg2 + seg3)>seg1))
+        {
+            istriangle = true;
+        }
+        else
+        {
+            istriangle=false;
+        }
+
+        // mostrar mensaje
+
+        if (istriangle)
+        {
+            Console.Write("el triangulo se puede construir");
+        }
+        else
+        {
+            Console.Write("el tringulo no se puede construir");
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
