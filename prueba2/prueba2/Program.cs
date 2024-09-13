acfd770f9a4cd21525edba4e2bd4c9377fa2d0c5
Romanechavarria <romandejesus19@hotmail.com>
2024-09-13 06:14:43 -0500

    se guardan cambios de numeros par

--- prueba2/prueba2/Program.cs
+++ prueba2/prueba2/Program.cs
@@ -2,6 +2,41 @@
 {
     private static void Main(string[] args)
     {
-        // este es un ejercio para prueba exsame
+        // Construir un programa que pida un número y luego diga si este número es par o no.
+
+        //declarar variables
+
+        double numero;
+        bool espar;
+
+        //pedir datos por pantalla
+
+        Console.WriteLine("ingrese el numero");
+        numero = Convert.ToDouble(Console.ReadLine());
+
+        //calcular el proceso
+
+        if (numero %2 == 0)
+        {
+            espar = true;
+        }
+        else
+                {
+                  espar= false;
+                 }
+
+        // mostrar el mensaje
+
+        if (espar)
+
+        {
+           Console.WriteLine("el numero es par");
+        }
+        else
+        {
+            Console.WriteLine("el numero no es par");
+        }
+
+
     }
 }
