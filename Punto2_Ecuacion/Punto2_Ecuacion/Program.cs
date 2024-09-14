ecfd31d94e2ebd96ae56eb7ad6e12bd0901bd6a6
Romanechavarria <romandejesus19@hotmail.com>
2024-09-13 22:11:11 -0500

    se guardan cambios de la solucion de la ecuacion

---
+++ Punto2_Ecuacion/Punto2_Ecuacion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Escriba un programa que pida los coeficientes de una ecuación de primer grado 
+         (a x + b = 0) y muestre la solución.
+        Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los números sean solución. La fórmula de las soluciones es x = -b / a*/
+
+        // declatar variables
+
+        double a, x, b;
+
+        //pedir datos por pantalla
+        Console.Write("ingrese el coeficiente a ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el coeficiente b ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el coeficiente x ");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        // resolver la ecuacion
+        if (a!= 0)
+        {
+            x = -b / a;
+
+            Console.WriteLine($"la solucion de la ecuacion es: x = {x}");
+
+        }
+        else if (b == 0)
+
+        {
+            Console.WriteLine("todos los numeros son la solucion");
+        }
+        else
+        {
+            Console.WriteLine("la ecuancion no tiene solucion");
+        }
+    }
+}
