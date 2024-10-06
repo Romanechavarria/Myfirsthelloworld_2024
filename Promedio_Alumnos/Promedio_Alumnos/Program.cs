5529d2c0c4932523b0529e7e43b9260abf5e986d
Romanechavarria <romandejesus19@hotmail.com>
2024-10-05 21:57:28 -0500

    se inicia programa ..

---
+++ Promedio_Alumnos/Promedio_Alumnos/Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Realizar un programa en C# que obtenga el promedio de calificaciones de un grupo de N alumnos.
+    }
+}
