bca0289ce2fd20d2018de0cd4dd2b289aefdea03
Romanechavarria <romandejesus19@hotmail.com>
2024-10-07 22:11:46 -0500

    se guardan cambios finales

--- Juego_Parcial2/Juego_Parcial2/Program.cs
+++ Juego_Parcial2/Juego_Parcial2/Program.cs
@@ -13,8 +13,70 @@ public class Program
             Console.WriteLine("Bienvenidos al juego 'Adivina el número'!");
 
             int jugadores = 0;
+
             // Elegir el número de jugadores (mínimo 2 y máximo 4)
+            do
+            {
+                Console.Write("Ingrese el número de jugadores (entre 2 y 4): ");
+                jugadores = Convert.ToInt32(Console.ReadLine());
+            } while (jugadores < 2 || jugadores > 4);  // se repite mientras el numero dejudadores este entre el rango
+
+            // Definir el rango del numero secreto
+
+            int rangoMax = jugadores switch
+            {
+                2 => 50,
+                3 => 100,
+                4 => 200,
+                _ => 50
+            };
+
+            // Generar el número aleatorio en el rango establecido
+
+            int numeroSecreto = random.Next(0, rangoMax + 1); // el numero secreto esta entre cero y 0 y rango maximo
+
+            bool acertado = false;  // controlar si alguien adivino el numero 
+
            
+
+            // Comienza el juego de adivinanza, este se repite hasta que alguiente adivine el numero
+            while (!acertado)
+            {
+                for (int i = 1; i <= jugadores; i++)
+                {
+                    Console.WriteLine($"\nTurno del Jugador #{i}");
+                    Console.Write("Ingrese un número: ");
+                    int intento = Convert.ToInt32(Console.ReadLine());
+
+                    // verifico si el numero ingresado es igual al numero secreto
+
+                    if (intento == numeroSecreto)
+                    {
+                        Console.WriteLine("¡HAS GANADO!");
+                        acertado = true;
+                        break;
+                    }
+                    else if (intento > numeroSecreto)
+                    {
+                        Console.WriteLine("MENOR");
+                    }
+                    else
+                    {
+                        Console.WriteLine("MAYOR");
+                    }
+                }
+
+                
+            }
+
+            // Preguntar si quieren jugar otra vez
+            Console.WriteLine("\n¿Desean jugar otra vez? (S/N)");
+            string respuesta = Console.ReadLine().ToUpper();
+
+            if (respuesta == "N")
+            {
+                continuar = false;
+            }
         }
     }
 }
