4f6c0370666fa6aedae8261954406f49cb5df0d3
Romanechavarria <romandejesus19@hotmail.com>
2024-10-07 21:47:02 -0500

    se guardan cambios

--- Juego_Parcial2/Juego_Parcial2/Program.cs
+++ Juego_Parcial2/Juego_Parcial2/Program.cs
@@ -1,8 +1,20 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        
-        Console.WriteLine("Hello, World!");
+        bool continuar = true;  // controla si el juego se repite o no
+        Random random = new Random(); // para generar numeros aleatorios 
+
+        while (continuar)  // reinicia el juego mientras que continuar siga siendo verdadero
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenidos al juego 'Adivina el número'!");
+
+            int jugadores = 0;
+            // Elegir el número de jugadores (mínimo 2 y máximo 4)
+           
+        }
     }
 }
