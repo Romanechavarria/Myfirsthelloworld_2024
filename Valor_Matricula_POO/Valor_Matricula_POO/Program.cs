5064269ad9d8a97b0e4ee3a18567e18fdbe74d28
Romanechavarria <romandejesus19@hotmail.com>
2024-10-30 07:41:55 -0500

    Se crea la clase estudiantes y se definen sus propidedades

--- Valor_Matricula_POO/Valor_Matricula_POO/Program.cs
+++ Valor_Matricula_POO/Valor_Matricula_POO/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Se desea obtener el valor de la matrícula de un estudiante cuyo valor se calcula de la siguiente manera:
+· Si toma 20 o menos créditos, paga el crédito al valor normal.
+· Si toma por encima de 20 créditos, se pagarán los créditos extras al doble de valor normal.
+· Si el estudiante es de estrato 1, 2 o 3 recibe los siguientes descuentos:
+- Si el estrato es 1, el descuento es del 80%.
+- Si el estrato es 2, el descuento es del 50%.
+- Si el estrato es 3, el descuento es del 30%.
+Además, los estratos 1 y 2 reciben subsidio de alimentación y transporte de la siguiente manera:
+· Para el estrato 1, el subsidio de alimentación y transporte es igual a $200.000.
+· Para el estrato 2, el subsidio de alimentación y transporte es igual a $100.000.
+Se debe informar al usuario sobre el costo de la matrícula y el valor del subsidio.
+La aplicación debe preguntar al usuario si quiere volver al inicio o no para calcular la matrícula de un nuevo estudiante.
+Hacer uso de todos los elementos de programación que hemos visto hasta el momento, incluyendoPOO (clases, propiedades, métodos)*/
+
+        
+    }
+}
