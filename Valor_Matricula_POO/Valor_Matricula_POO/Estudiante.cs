26f88282f3f8e4fde3973e48394d9342baa7b6c6
Romanechavarria <romandejesus19@hotmail.com>
2024-10-30 08:25:41 -0500

    crearmos el condicional para hacer el calculo

--- Valor_Matricula_POO/Valor_Matricula_POO/Estudiante.cs
+++ Valor_Matricula_POO/Valor_Matricula_POO/Estudiante.cs
@@ -14,6 +14,36 @@ namespace EjercicioMatrícula
         {
             decimal costoMatricula = 0;
 
-           
+            // Calcular el costo de los créditos
+            if (Creditos <= 20)
+            {
+                costoMatricula = Creditos * ValorCredito;
+            }
+            else
+            {
+                int creditosExtras = Creditos - 20;
+                costoMatricula = (20 * ValorCredito) + (creditosExtras * ValorCredito * 2);
+            }
+
+            // Aplicar descuento de acuerdo al estrato
+            decimal descuento = 0;
+            switch (Estrato)
+            {
+                case 1:
+                    descuento = 0.80m;
+                    break;
+                case 2:
+                    descuento = 0.50m;
+                    break;
+                case 3:
+                    descuento = 0.30m;
+                    break;
+            }
+            costoMatricula -= costoMatricula * descuento;
+
+            return costoMatricula;
+        }
+
+        
     }
 }
