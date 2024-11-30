32d33f7d5d2ecf6bb2d763ee70644f24b74dbebc
Romanechavarria <romandejesus19@hotmail.com>
2024-11-29 20:42:54 -0500

    se define la dimenciones de la matriz

---
+++ Juego_Parcial_Final/Juego_Parcial_Final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Definir las dimensiones de la matriz 5x5
+        int rows = 5;
+        int cols = 5;
+
+    }
+}
