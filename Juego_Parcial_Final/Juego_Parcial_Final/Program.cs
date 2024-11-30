ab1997c54935a55ffd7be333a46f690128579a96
Romanechavarria <romandejesus19@hotmail.com>
2024-11-29 20:50:38 -0500

    se crea la matriz y se le indican los paramatros a mostrar

--- Juego_Parcial_Final/Juego_Parcial_Final/Program.cs
+++ Juego_Parcial_Final/Juego_Parcial_Final/Program.cs
@@ -8,5 +8,19 @@ public class Program
         int rows = 5;
         int cols = 5;
 
+        // Crear la matriz
+        int[,] matrix = new int[rows, cols];
+        int currentRow = 0, currentCol = 0; // Iniciar en la posición (0,0)
+        
+        // Llenar la matriz con números aleatorios
+        FillMatrix(matrix);
+
+        // Mostrar la matriz inicial
+        DisplayMatrix(matrix);
+
+        // Esperar la tecla ESC para salir
+        WaitForExit(matrix, ref currentRow, ref currentCol);
+    }
+
     }
 }
